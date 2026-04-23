cc1e9da0ade2e63a8c21e92bfbbd71f07f36bc32
sudarsinianisetti <sudarsinianisetti@gmail.com>
2026-04-23 19:40:50 +0530

    Add files via upload

---
+++ Volume of Sphere.py
@@ -0,0 +1,6 @@
+Python 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+... r=int(input())
+... pi=3.14
+... volume=(4/3) * pi *r**3
